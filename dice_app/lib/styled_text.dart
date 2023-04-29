95ee4520599df3137c1d18d9947c4bb13b3e6f23
oandeliz <59943720+oandeliz@users.noreply.github.com>
2023-04-29 14:09:59 -0400

    learning how to build containers and states

---
+++ dice_app/lib/styled_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return const Text(
+      'Hello World',
+      style: TextStyle(color: Colors.white, fontSize: 28.0),
+    );
+  }
+}
